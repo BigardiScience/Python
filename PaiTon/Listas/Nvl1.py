5489f9188a425e493a8dbe601b193f83d6cb1fd7
BigardiScience <guilhermebigardimessias01@gmail.com>
2025-10-28 16:46:50 -0300

    Delete PaiTon directory

--- PaiTon/Listas/Nvl1.py
+++
@@ -1,8 +0,0 @@
-lista = []
-num = int(input("Quantos itens quer cadastrar? "))
-
-for i in range(1,num + 1):
-    Itens = str(input("Qual o nome do item? "))
-    lista.append ([Itens])
-
-print(lista)
