48df027f335302d44431e7b9e78bbe832d4418e3
BigardiScience <guilhermebigardimessias01@gmail.com>
2025-10-28 14:33:03 -0300

    Nvl3.py

--- PaiTon/Tuplas e Dicionários/Nvl3.py
+++ PaiTon/Tuplas e Dicionários/Nvl3.py
@@ -1,1 +1,9 @@
+lista = [("Maca", 8,2),
+         ("Pêra",5,8),
+         ("Banana",10,5)]
 
+for produto in lista: #“Percorra cada item da lista, um por um, e chame esse item dentro de cada tupla de numero enquanto estiver dentro do laço.”
+    nome = produto[0]
+    preco = produto[1]
+    quantidade = produto[2]
+    print(f"O produto {nome} custa R${preco} e há {quantidade} disponíveis em estoque.")
