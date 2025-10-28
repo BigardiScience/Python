01a620a61dd172ea196aac6d7716086d49b5ad80
BigardiScience <guilhermebigardimessias01@gmail.com>
2025-10-28 11:32:07 -0300

    Delete PaiTon directory

--- PaiTon/Def/Nvl1.py
+++
@@ -1,8 +0,0 @@
-
-def soma(num,num2):
-    resultado = num+num2
-    return resultado
-
-num = int(input("Digite o primeiro número para a soma: "))
-num2 = int(input("Digite o segundo número para a soma: "))
-print("A soma de ",num," + ",num2, " = ", soma(num,num2))
