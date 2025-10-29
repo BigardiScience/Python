ad0ab52de2689a45fa94647fe4142f08dda9fc63
BigardiScience <guilhermebigardimessias01@gmail.com>
2025-10-29 12:06:59 -0300

    Update Nvl6.py

--- PaiTon/Tuplas e Dicionários/Nvl6.py
+++ PaiTon/Tuplas e Dicionários/Nvl6.py
@@ -1,3 +1,4 @@
+#Modelo Iniciante seus arrombados
 dados = []
 
 while True:
@@ -15,3 +16,34 @@ for busca in dados:
     idade = busca[1]
     cargo = busca[2]
     print(f"Cadastrado: {nome} : {idade} : {cargo}")
+
+
+
+#Esse modelo abaixo é mais avançado
+dados = []
+def cadastro():
+    while True:
+        nome = input("Digite o nome para cadastro: ")
+        idade = int(input("Digite a idade: "))
+        cargo = input("Cargo: ")
+        dados.append((nome,idade,cargo))
+        print(f"O {nome} com {idade} anos de idade foi cadastrado no cargo de {cargo}")
+        choose = input(f"Deseja continuar cadastrando? ")
+        if choose.lower() != "sim":
+            break
+    return f"Visão Geral : {dados}"
+print(cadastro())
+
+for indice, busca in enumerate(dados, start=1):
+    nome = busca[0]
+    idade = busca[1]
+    cargo = busca[2]
+    print(f"{indice} - {nome} , {idade} , {cargo}")
+    opcaoInicio = input(f"Deseja retornar ao inicio? ")
+    if opcaoInicio.lower() != "sim":
+        print("Fim do Programa! ")
+        break
+    else:
+        print(cadastro())
+    
+
