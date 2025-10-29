82aebd4c5fadb28eb4456207fcb078ecbd75be18
BigardiScience <guilhermebigardimessias01@gmail.com>
2025-10-29 11:41:03 -0300

    Update Nvl6.py

--- PaiTon/Tuplas e Dicionários/Nvl6.py
+++ PaiTon/Tuplas e Dicionários/Nvl6.py
@@ -1,1 +1,17 @@
+dados = []
 
+while True:
+    nome = input("Digite o nome para cadastro: ")
+    idade = int(input("Digite a idade: "))
+    cargo = input("Cargo: ")
+    dados.append((nome,idade,cargo))
+    print(f"O {nome} com {idade} anos de idade foi cadastrado no cargo de {cargo}")
+    choose = input(f"Deseja continuar cadastrando? ")
+    if choose.lower() != "sim":
+        break
+
+for busca in dados:
+    nome = busca[0]
+    idade = busca[1]
+    cargo = busca[2]
+    print(f"Cadastrado: {nome} : {idade} : {cargo}")
